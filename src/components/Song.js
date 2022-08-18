d4a433c5af822439748bd07e786c27d7c7eddd9b
Aydar Fayzullin <faidarik@mail.ru>
2022-08-18 00:42:36 -0400

    create basic layout

---
+++ src/components/Song.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Song = () => {
+  return (
+    <div className="song-container">
+      <h1>Pic</h1>
+      <p>Song Name</p>
+    </div>
+  );
+};
+
+export default Song;
